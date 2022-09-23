817b0ddac9091420d352778f5a7a9d5321fa0614
Jonathan Levi Carrillo Portillo <SMSS019222@ugb.edu.sv>
2022-09-23 17:13:19 -0600

    Agregar archivos de proyecto.

---
+++ Semana8Ejercicio1/Form2.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Semana8Ejercicio1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMensaje1 = new System.Windows.Forms.Label();
+            this.btnClouse = new System.Windows.Forms.Button();
+            this.lblMensaje2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMensaje1
+            // 
+            this.lblMensaje1.AutoSize = true;
+            this.lblMensaje1.Location = new System.Drawing.Point(59, 39);
+            this.lblMensaje1.Name = "lblMensaje1";
+            this.lblMensaje1.Size = new System.Drawing.Size(35, 13);
+            this.lblMensaje1.TabIndex = 0;
+            this.lblMensaje1.Text = "label1";
+            // 
+            // btnClouse
+            // 
+            this.btnClouse.Location = new System.Drawing.Point(62, 93);
+            this.btnClouse.Name = "btnClouse";
+            this.btnClouse.Size = new System.Drawing.Size(75, 23);
+            this.btnClouse.TabIndex = 1;
+            this.btnClouse.Text = "Cerrar";
+            this.btnClouse.UseVisualStyleBackColor = true;
+            this.btnClouse.Click += new System.EventHandler(this.btnClouse_Click);
+            // 
+            // lblMensaje2
+            // 
+            this.lblMensaje2.AutoSize = true;
+            this.lblMensaje2.Location = new System.Drawing.Point(291, 39);
+            this.lblMensaje2.Name = "lblMensaje2";
+            this.lblMensaje2.Size = new System.Drawing.Size(35, 13);
+            this.lblMensaje2.TabIndex = 2;
+            this.lblMensaje2.Text = "label1";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 158);
+            this.Controls.Add(this.lblMensaje2);
+            this.Controls.Add(this.btnClouse);
+            this.Controls.Add(this.lblMensaje1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMensaje1;
+        private System.Windows.Forms.Button btnClouse;
+        private System.Windows.Forms.Label lblMensaje2;
+    }
+}
---
+++ Semana8Ejercicio1/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semana8Ejercicio1
+{
+    public partial class Form2 : Form
+
+    {
+        public string mensaje1;
+        public Form2(string mensaje)
+        {
+            InitializeComponent();
+            lblMensaje2.Text = mensaje;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            lblMensaje1.Text = mensaje1;
+        }
+
+        private void btnClouse_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
